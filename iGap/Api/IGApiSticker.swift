895eb3f333cab63b8f51b71e8d18ad7d02ba647c
SaeedMozaffari <mozaffari.saeed10@gmail.com>
2019-02-05 14:17:22 +0330

    Improvement (Sticker) - Get Sticker Info get sticker info from server

---
+++ iGap/Api/IGApiSticker.swift
@@ -0,0 +1,68 @@
+/*
+ * This is the source code of iGap for iOS
+ * It is licensed under GNU AGPL v3.0
+ * You should have received a copy of the license in this archive (see LICENSE).
+ * Copyright © 2017 , iGap - www.iGap.net
+ * iGap Messenger | Free, Fast and Secure instant messaging application
+ * The idea of the Kianiranian STDG - www.kianiranian.com
+ * All rights reserved.
+ */
+
+import Foundation
+import Alamofire
+
+public class IGApiSticker {
+    
+    static let shared = IGApiSticker()
+    
+    private let url = "https://sticker.igap.net/stickers/favorite"
+    
+    
+    public func callStickerApi() {
+        Alamofire.request(url, headers: headers).responseStickerApi { response in
+            if let stickerApi = response.result.value {
+                IGFactory.shared.setSticker(stickers: stickerApi.data)
+            }
+        }
+    }
+    
+    func newJSONDecoder() -> JSONDecoder {
+        let decoder = JSONDecoder()
+        if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+            decoder.dateDecodingStrategy = .iso8601
+        }
+        return decoder
+    }
+    
+    func newJSONEncoder() -> JSONEncoder {
+        let encoder = JSONEncoder()
+        if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+            encoder.dateEncodingStrategy = .iso8601
+        }
+        return encoder
+    }
+}
+
+extension DataRequest {
+    fileprivate func decodableResponseSerializer<T: Decodable>() -> DataResponseSerializer<T> {
+        return DataResponseSerializer { _, response, data, error in
+            guard error == nil else { return .failure(error!) }
+            
+            guard let data = data else {
+                return .failure(AFError.responseSerializationFailed(reason: .inputDataNil))
+            }
+            
+            return Result { try JSONDecoder().decode(T.self, from: data) }
+        }
+    }
+    
+    @discardableResult
+    fileprivate func responseDecodable<T: Decodable>(queue: DispatchQueue? = nil, completionHandler: @escaping (DataResponse<T>) -> Void) -> Self {
+        return response(queue: queue, responseSerializer: decodableResponseSerializer(), completionHandler: completionHandler)
+    }
+    
+    @discardableResult
+    func responseStickerApi(queue: DispatchQueue? = nil, completionHandler: @escaping (DataResponse<StickerApi>) -> Void) -> Self {
+        return responseDecodable(queue: queue, completionHandler: completionHandler)
+    }
+}
